7887193b446adc15f1b133e5c43f43b5c1d8e815
ennouhix <127449312+ennouhix@users.noreply.github.com>
2024-03-02 17:31:15 +0100

    v!
    vf

---
+++ database/migrations/2023_12_10_190454_create_orders_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+        
+            
+            $table->id();
+            $table->string('professeur');
+            $table->string('impression_type');
+            $table->string('filiere');
+            $table->integer('ncopies');
+            $table->string('format');
+            $table->string('print_type');
+            $table->string('color');
+            $table->string('print_style');
+            $table->datetime('datetime');
+            $table->string('pdf_path');
+            $table->timestamps();
+            $table->string('status')->default('en_attente');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2023_12_17_194802_create_commandes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Professeur');
+            $table->string('Impression');
+            $table->string('Filiere');
+            $table->integer('NCopies');
+            $table->string('Format');
+            $table->string('Type');
+            $table->string('Couleur');
+            $table->string('TImpression');
+            $table->datetime('DateTime');
+            $table->string('pdfFile'); // Assuming you store the file name 
+            $table->timestamps();
+            $table->string('status')->default('en_attente');
+            
+
+
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
